98f1a6b0c7d7ddd174dec89c93e8e573d553b78c
Md irshad Alam <113013301+MyHobbies0@users.noreply.github.com>
2023-05-12 00:57:28 +0600

    Update db.js

--- database/db.js
+++ database/db.js
@@ -11,7 +11,7 @@ async function connectDatabase() {
   
   async function  userdatabase() {
     try {
-      const result = await mongoose.connect(`mongodb+srv://${config.username}:${config.password}@cluster0.usgs7zg.mongodb.net/?retryWrites=true&w=majority`);
+      const result = await mongoose.connect(`mongodb+srv://irshad:irshad1234@cluster0.usgs7zg.mongodb.net/?retryWrites=true&w=majority`);
      console.log(" user connected")
       return result;
     } catch (error) {
