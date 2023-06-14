5fae600fef9318c2a25dc093225e976eb79af2b3
Md irshad Alam <113013301+MyHobbies0@users.noreply.github.com>
2023-06-14 16:15:23 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -4,8 +4,8 @@ mongoose.set('strictQuery', true);
 
   async function  userdatabase() {
    
-      // const result = await mongoose.connect(`mongodb+srv://irshad:irshad1234@cluster0.usgs7zg.mongodb.net/?retryWrites=true&w=majority`);
-      const result = await mongoose.connect(`mongodb://0.0.0.0:27017/AllChat`);
+      const result = await mongoose.connect(`mongodb+srv://irshad:irshad1234@cluster0.usgs7zg.mongodb.net/?retryWrites=true&w=majority`);
+    
       console.log(" user connected")
     return result;
   }
